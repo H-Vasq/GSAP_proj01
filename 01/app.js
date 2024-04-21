46f4c0420afddcff1c70e390f1a817acecb40739
Henrik Vasquez <motionbuilder@yahoo.com>
2024-04-21 13:25:25 -0600

    animate circle shape opacity, position, color

--- 01/app.js
+++ 01/app.js
@@ -1,6 +1,20 @@
+// gsap.to(".box", {
+//     // x: -100,
+//     y: 200,
+//     duration: 2,
+//     repeat: 2
+// })
+
+gsap.set(".box", {
+    opacity: 0,
+    background: "yellow",
+});
+
 gsap.to(".box", {
-    x: -100,
-    y: 200,
-    duration: 2,
-    repeat: 2
+    opacity: 1,
+    background: "crimson",
+    duration: 3,
+    y: 100,
+    repeat: -1,
+    yoyo: true
 })
