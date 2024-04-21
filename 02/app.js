946fc335a0c5ac7861ff6e7e48fe4f091141b5e0
Henrik Vasquez <motionbuilder@yahoo.com>
2024-04-21 13:36:57 -0600

    start .from method lesson

--- 02/app.js
+++ 02/app.js
@@ -5,16 +5,24 @@
 //     repeat: 2
 // })
 
-gsap.set(".box", {
-    opacity: 0,
-    background: "yellow",
-});
+// gsap.set(".box", {
+//     opacity: 0,
+//     background: "yellow",
+// });
 
-gsap.to(".box", {
-    opacity: 1,
-    background: "crimson",
+// gsap.to(".box", {
+//     opacity: 1,
+//     background: "crimson",
+//     duration: 3,
+//     y: 100,
+//     repeat: -1,
+//     yoyo: true
+// })
+
+gsap.from(".box1", {
+    y: -200,
     duration: 3,
-    y: 100,
+    ease: "linear",
     repeat: -1,
     yoyo: true
 })
