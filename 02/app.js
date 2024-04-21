e29e22cf67d392e7aa9c01281db57c8408322396
Henrik Vasquez <motionbuilder@yahoo.com>
2024-04-21 13:41:05 -0600

    add .fromTo

--- 02/app.js
+++ 02/app.js
@@ -19,10 +19,22 @@
 //     yoyo: true
 // })
 
-gsap.from(".box1", {
+// gsap.from(".box1", {
+//     y: -200,
+//     duration: 3,
+//     ease: "linear",
+//     repeat: -1,
+//     yoyo: true
+// })
+
+gsap.fromTo(".box2", {
+    y: 200, opacity: 0,
+}, {
+    opacity: 1,
     y: -200,
     duration: 3,
     ease: "linear",
+    borderRadius: 0,
     repeat: -1,
     yoyo: true
 })
